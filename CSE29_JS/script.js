625ae00d3f2e215275678f56e0c7656ddf07750c
nkshhh11 <pv9696589211@gmail.com>
2026-02-25 14:12:58 +0530

    introduction to data types in java script

---
+++ CSE29_JS/script.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+document.write("hello world");
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console .assert;ongamepadconnected(num)
+console.log(typeof(num));
+
+
+
+const sym1 = symbol(4);
+console.log(sym1)
+const sym2 = symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true")
+}
+else{
+    console.log("FALSE ")
+}
