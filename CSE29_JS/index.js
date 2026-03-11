fe66ec3f075d8904f80690cec4ca68ef7c3e58e9
nkshhh11 <pv9696589211@gmail.com>
2026-03-11 14:50:52 +0530

    final index

--- CSE29_JS/index.js
+++ CSE29_JS/index.js
@@ -26,3 +26,52 @@
 //     document.write("<b>you are eligible to cast the vote</b>")
 // }
 
+
+// var grade='A';
+// document.write("Entering switch block<br/>");
+// switch(grade){
+//     case 'A':document.write("Good job<br/");
+//     break;
+
+//     case 'B':document.write("Pretty good<br/>");
+//     break;
+
+//     case 'C':document.write("Passed<br/>");
+//     break;
+
+//     default:document.write("Unknown grade<br/>")
+// }
+
+
+// function myfunction(){
+//     alert("hellow world")
+// }
+// function sayhello(name , age){
+//     document.write(name+is+age+years old)
+// }
+
+
+// var add2=(a,b)=>{
+//     console.log(a+b);
+// }
+// add2(5,6);
+
+
+
+//object is the collection of anything 
+// representation by curly bracket
+
+
+
+var person={
+    name: 'john',
+    age: 30,
+    ismarried : true,
+    address:{
+        street: 'vinay nagar',
+        flatNO:201
+    }
+}
+console.log(person);
+
+//array is a special type of object
