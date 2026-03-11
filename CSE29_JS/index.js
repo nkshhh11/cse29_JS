72d39fe42620242849bbd0a4ff8a765e04906c28
nkshhh11 <pv9696589211@gmail.com>
2026-03-11 14:50:00 +0530

    index

---
+++ CSE29_JS/index.js
@@ -0,0 +1,28 @@
+
+// var count;
+// document.write("starting Loop <br>");
+
+// for(count = 0; count < 10; count++){
+//     document.write("current count : " + count);
+//     document.write("<br>");
+// }
+
+// document.write("loop stopped");
+
+
+
+// const person={
+//     fname:"ajay",
+//     lname:"singh",
+//     age:23
+// };
+// for(let x in person){
+//   console.log("person details: "+x+person[x]);
+// }
+
+
+// var age =20;
+// if(age>18){
+//     document.write("<b>you are eligible to cast the vote</b>")
+// }
+
